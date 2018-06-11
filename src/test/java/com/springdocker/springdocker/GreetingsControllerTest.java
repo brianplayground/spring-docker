5db3f4b4bf7d8c00e301162c57c41491e943a07c
BrianPhiri <brianphiri.9523@gmail.com>
2018-06-11 21:43:37 +0300

    tests

---
+++ src/test/java/com/springdocker/springdocker/GreetingsControllerTest.java
@@ -0,0 +1,23 @@
+package com.springdocker.springdocker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(GreetingsController.class)
+public class GreetingsControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void helloTest() throws Exception {
+        mockMvc.perform(get("/hello")).andExpect(status().isOk());
+    }
+}
